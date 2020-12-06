d4959b6b54f46f0c9b7ce2f404bd67d4d7ac4f7e
Daniella Bojado <daniella.bojado@gmail.com>
2020-12-05 23:04:55 -0600

    add

--- acquire.py
+++ acquire.py
@@ -9,20 +9,14 @@ import os
 
 ################### Acquire Nutritional Facts Data #####################
 
-def nutrition_data():
-    '''
-    '''
-    df('nutrition.csv')
-    return df
-
-
 def get_nutrition_data(cached=False):
     '''
     This function reads nutrition data if cached == False
     or if cached == True reads in nutrition data df from a csv file, returns df
     '''
     if cached or os.path.isfile('nutrition.csv') == False:
-        df = zillow_data()
+        df = nutrition()
     else:
-        df = pd.read_csv('nutrition.csv, index_col=0)
+        df = pd.read_csv('nutrition.csv')
     return df
+
--- prepare.py
+++ prepare.py
@@ -20,7 +20,7 @@ def clean_nutrition_data():
 
     # Drop unnecessary columns
     df = df.drop(columns=cols_to_drop)
-    df = prepare.data_prep(df,cols_to_remove=['id', ''],
+    df = prepare.data_prep(df,cols_to_remove=['id'],
     prop_required_column=.6,
     prop_required_row=.75)
 
@@ -28,8 +28,8 @@ def clean_nutrition_data():
     df = df.dropna(axis=1, thresh=threshold, inplace=True)
 
     # Specific removal of outliers
-    df = df[]
-    return df
+    # df = df[]
+    # return df
 
 def prep_nutrition_data():
     df = clean_data()
